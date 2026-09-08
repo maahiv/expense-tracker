63b301cb89026c8430117c69bd0460a6223bb3c7
Project Developer <developer@localhost>
2026-09-08 16:01:07 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-// Firebase project configuration used by the Expense Tracker app.
 const firebaseConfig = {
-  apiKey: 'AIzaSyD9ZtHQbZo5OCRjWAxGCpcAoql-VxTdIrY',
-  authDomain: 'expense-tracker-signup-2636e.firebaseapp.com',
-  projectId: 'expense-tracker-signup-2636e',
-  storageBucket: 'expense-tracker-signup-2636e.firebasestorage.app',
-  messagingSenderId: '556684472867',
-  appId: '1:556684472867:web:765cc4ea563b302923b796',
-  measurementId: 'G-49Q4JC777E',
-  databaseURL: 'https://expense-tracker-signup-2636e-default-rtdb.firebaseio.com'
+  apiKey: "AIzaSyCtv5Y6IcO_keQ-ZxbPIlx0HzSCfnEIc1s",
+  authDomain: "expense-tracker-3e596.firebaseapp.com",
+  projectId: "expense-tracker-3e596",
+  storageBucket: "expense-tracker-3e596.firebasestorage.app",
+  messagingSenderId: "616933221447",
+  appId: "1:616933221447:web:fc6a9b9e628ef34060bd62"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
